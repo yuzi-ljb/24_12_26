46865b48655c045d9e7ec73f19efc7498f253a71
佳斌 李 <l3234216896@gmail.com>
2024-12-26 20:02:26 +0800

    1

--- 24_12_26/test.c
+++ 24_12_26/test.c
@@ -1,12 +1,31 @@
 #include<stdio.h>
 
-int main()
-{
-	printf("hehe\n");
-	printf("hehe\n");
-	printf("hehe\n");
-	printf("hehe\n");
-	printf("hehe\n");
-	printf("hehe\n");
+//int main()
+//{
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	return 0;
+//  }
+//比较古老的函数
+//void main()
+//{
+//	printf("hehe\n");
+//
+//	return 0;
+//}
+
+
+int main() {
+
+
+	printf("%d\n",6);
+	printf("%f\n", 3.14);
+	printf("%c\n", 'a');
+	printf("%s\n", "asdfghjkl");
 	return 0;
 }
+
