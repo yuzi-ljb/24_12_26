3a67f54dbc5839dbd0784f6976fd5d916dc3864d
佳斌 李 <l3234216896@gmail.com>
2025-01-04 01:54:13 +0800

    1/4 c语言常见概念

--- 24_12_26/test.c
+++ 24_12_26/test.c
@@ -22,9 +22,22 @@
 int main() {
 
 
-	printf("%d\n",6);
+	/*printf("%d\n",6);
 	printf("%f\n", 3.14);
 	printf("%c\n", 'a');
+	printf("%s\n", "asdfghjkl");*/
+	/*printf("%d\n",'a');
+	printf("%c\n", 97);*/
+	/*int i = 0;*/
+	/*for (i = 32; i <= 127; i++)
+	{
+
+		printf("%c ", i);
+
+	}*/
+	
+	printf("%s\n", "asdfghjkl");
+	printf("%s\n", "asdfghjkl");
 	printf("%s\n", "asdfghjkl");
 	return 0;
 }
